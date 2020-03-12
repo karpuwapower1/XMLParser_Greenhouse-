a0c4afefdb68d47ab0a84ea50235523d2aea2894
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-03-12 14:28:03 +0300

    Command interface was created

---
+++ src/main/java/by/training/task04/karpilovich/command/Command.java
@@ -0,0 +1,16 @@
+package by.training.task04.karpilovich.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import by.training.task04.karpilovich.resource.PageManager;
+
+public interface Command {
+	
+	public PageManager execute(HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException;
+
+}
