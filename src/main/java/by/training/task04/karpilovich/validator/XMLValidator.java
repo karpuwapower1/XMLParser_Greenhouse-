f968553f62615711e6de5226377ea5a8b08735d3
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-03-12 14:37:23 +0300

    Validator was refactored

--- src/main/java/by/training/task04/karpilovich/validator/XMLValidator.java
+++ src/main/java/by/training/task04/karpilovich/validator/XMLValidator.java
@@ -2,6 +2,7 @@ package by.training.task04.karpilovich.validator;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
@@ -9,18 +10,30 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.xml.sax.SAXException;
 
+import by.training.task04.karpilovich.exception.ParserException;
+
 public class XMLValidator {
-	
-	public boolean validate() throws SAXException, IOException {
-		SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
-		File fileSchema = new File(getClass().getClassLoader().getResource("flowers.xsd").getPath());
-		Schema schema = factory.newSchema(fileSchema);
-		Validator validator = schema.newValidator();
-		Source source = new StreamSource(new File(getClass().getClassLoader().getResource("flowers.xml").getPath()));
-		validator.validate(source);
-		
-		return true;
+
+	private static final String SCHEMA_FILE_NAME = "flowers.xsd";
+	private static final String SCHEMA_LANGUAGE = "http://www.w3.org/2001/XMLSchema";
+	private static final Logger LOGGER = LogManager.getLogger(XMLValidator.class);
+
+	public void validate(File file) throws ParserException {
+		try {
+			SchemaFactory factory = SchemaFactory.newInstance(SCHEMA_LANGUAGE);
+			URL url = getClass().getClassLoader().getResource(SCHEMA_FILE_NAME);
+			LOGGER.debug(url == null);
+			Schema schema = factory.newSchema(url);
+			Validator validator = schema.newValidator();
+			Source source = new StreamSource(file);
+			validator.validate(source);
+		} catch (SAXException | IOException e) {
+			LOGGER.warn("Illegal file " + e);
+			throw new ParserException("Illegal file ", e);
+		}
 	}
 }
